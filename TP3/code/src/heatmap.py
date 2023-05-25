e5b7cb3a8b9ed1673d58a8f6c931a223089771cf
hassantorkaman <92051279+hassantorkaman@users.noreply.github.com>
2023-05-24 20:59:52 -0400

    1

--- TP3/code/src/heatmap.py
+++ TP3/code/src/heatmap.py
@@ -23,4 +23,12 @@ def get_figure(data):
     # TODO : Create the heatmap. Make sure to set dragmode=False in
     # the layout. Also don't forget to include the hover template.
 
-    return None
+    heatmap_fig = px.imshow(data,
+                            x=data.columns.year.tolist(), 
+                            y=data.index.tolist(),
+                            labels=dict(color="Trees",x="Year", y="Neighborhood"))
+    heatmap_fig.update_traces(hovertemplate=hover_template.get_heatmap_hover_template())
+    heatmap_fig.update_layout(dragmode=False)
+    heatmap_fig.update_layout(xaxis=dict(tickmode='linear'))
+    return heatmap_fig
+
